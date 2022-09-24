919752199e0fa676f2e9da8ae666b8dbd00b6d65
Z262664 <z262664@smmacboz262664m.gfsscorp.mx.bsch>
2022-09-24 17:42:59 -0500

    integration service

--- src/productos/components/ProductsList.jsx
+++ src/productos/components/ProductsList.jsx
@@ -1,50 +1,55 @@
-import { ProductCard } from "./ProductCard"
-import Grid from '@mui/material/Grid'
+import { ProductCard } from "./ProductCard";
+import Grid from "@mui/material/Grid";
+import { getProducts } from "../helpers";
+import { useEffect } from "react";
+import { useState } from "react";
 
-
-const products =[
-    {
-        name:'img',
-        price:50,
-        description:''
-    }, 
-    {
-        name:'img1',
-        price:50,
-        description:''
-    },
-    {
-        name:'img2',
-        price:50,
-        description:''
-    },
-    {
-        name:'img3',
-        price:50,
-        description:''
-    },
-    {
-        name:'img4',
-        price:50,
-        description:''
-    }
-];
+/* const products = [
+  {
+    name: "img",
+    price: 50,
+    description: "",
+  },
+  {
+    name: "img1",
+    price: 50,
+    description: "",
+  },
+  {
+    name: "img2",
+    price: 50,
+    description: "",
+  },
+  {
+    name: "img3",
+    price: 50,
+    description: "",
+  },
+  {
+    name: "img4",
+    price: 50,
+    description: "",
+  },
+]; */
 
 export const ProductsList = () => {
-  return (
+  const [products, setProducts] = useState([]);
 
+  const getProductInfo = async () => {
+    const newProducts = await getProducts("valorant");
+    setProducts(newProducts);
+  };
+  useEffect(() => {
+    getProductInfo();
+  }, []);
+
+  return (
     <Grid container>
-        {
-            products.map(producto=>(
-                <Grid item 
-                    key={producto.name}
-                    sx={{p:1}}
-                >
-                    <ProductCard />
-                </Grid>
-            ))
-        }      
-    </Grid>   
-   
-  )
-}
+      {products.map((productoItem) => (
+        <Grid item key={productoItem.id} sx={{ p: 1 }}>
+          <ProductCard {...productoItem} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
